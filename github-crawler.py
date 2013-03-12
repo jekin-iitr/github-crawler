08d004cd242b3a4064e6c26264049d26e2a7b952
Prakash Gamit <prakashgamit23@gmail.com>
2013-03-12 21:48:06 +0530

    get home page of user

---
+++ github-crawler.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Prakash Gamit <prakashgamit23@gmail.com>
+# Indian Institute of Technology, Roorkee
+
+from httplib import *
+import sys
+
+def getPage(url):
+    conn = HTTPSConnection('github.com')
+    conn.request('GET', url)
+
+    response = conn.getresponse()
+
+    conn.close()
+
+    if response.status == 200:
+        return response.read()
+    else:
+        print 'Error retrieving page from github'
+        return
+# end getPage()
+
+
+def main():
+    username = sys.argv[1]
+    homepage = getPage('/' + username)
